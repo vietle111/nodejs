189182ee1341cfca2a2c4e01d5b57d714080b1bb
vietlq <vietlq@galaxytelecom.vn>
2025-05-27 09:45:33 +0700

    update

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ const express = require("express");
 const connectDB = require("./src/config/db"); // Import file kết nối MongoDB
 const User = require("./src/models/User");
 const userRoutes = require("./src/routes/userRoutes"); // Import routes của User
+const authRoutes = require("./src/routes/authRoute"); // ✅ Thêm dòng này
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 
-// ✅ Kích hoạt CORS
-app.use(cors());  // Cho phép tất cả domain truy cập
+const user = {
+  id: "123456", // bạn có thể thay bằng ObjectId thật từ Mongo
+  username: "vietlequoc"
+};
 
+const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
 
+console.log(token);
+
+// ✅ Kích hoạt CORS (Chỉ cần một lần)
 app.use(cors({
   origin: "http://localhost:3000", // Chỉ cho phép frontend chạy ở cổng 3000
   methods: ["GET", "POST", "PUT", "DELETE"], // Các phương thức được phép
   allowedHeaders: ["Content-Type", "Authorization"], // Các headers được phép
 }));
+
 // Middleware để parse JSON
 app.use(express.json());
 
@@ -26,6 +35,7 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello from Node.js & MongoDB!");
 });
+app.use("/api/auth", authRoutes); // ✅ Thêm dòng này
 
 // API tạo user
 app.post("/users", async (req, res) => {
@@ -37,8 +47,10 @@ app.post("/users", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
 // Sử dụng routes cho User
 app.use("/api/users", userRoutes);
+
 // Lắng nghe server
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
---
+++ src/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require("../models/UserLogin");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
